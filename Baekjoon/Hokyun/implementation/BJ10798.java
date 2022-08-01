1cf07ce9fd4d3a48b96392cd0e3041f5c6892434
hokyun <h0kyungim@gmail.com>
2022-08-01 12:13:38 +0900

    add hokyun implementation

---
+++ Baekjoon/Hokyun/implementation/BJ10798.java
@@ -0,0 +1,31 @@
+package baekjoon.implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ10798 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[][] board = new char[5][];
+		String s;
+
+		for (int i = 0; i < 5; i++) {
+			s = br.readLine();
+			board[i] = s.toCharArray();
+		}
+
+		s = "";
+
+		for (int i = 0; i < 15; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (i >= board[j].length || board[j][i] == '\0')
+					continue;
+				s += board[j][i];
+			}
+		}
+		
+		System.out.println(s);
+	}
+
+}
---
+++ Baekjoon/Hokyun/implementation/BJ2490.java
@@ -0,0 +1,45 @@
+package baekjoon.implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ2490 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 0 배, 1 등, A 도, B 개, C 걸, D 윷, E 모
+		char result;
+		for (int i = 0; i < 3; i++) {
+			int oneCount = 0;
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			while (st.hasMoreTokens()) {
+				if (Integer.parseInt(st.nextToken()) == 1)
+					oneCount++;
+			}
+
+			switch (oneCount) {
+			case 0:
+				result = 'D';
+				break;
+			case 1:
+				result = 'C';
+				break;
+			case 2:
+				result = 'B';
+				break;
+			case 3:
+				result = 'A';
+				break;
+			default:
+				result = 'E';
+				break;
+
+			}
+
+			System.out.println(result);
+		}
+	}
+
+}
